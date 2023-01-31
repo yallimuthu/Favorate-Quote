0088735c32b73fd273e9f50f0280fa15304eb7d7
yallimuthu <63921803+yallimuthu@users.noreply.github.com>
2023-01-30 17:38:02 -0800

    Add files via upload

---
+++ Favorate Quote.java
@@ -0,0 +1,13 @@
+/*
+    Favorate Quote
+*/
+class FavorateQuote{
+    // A Java program begins with a call to main()
+    public static void main(String[] args) {
+        String output = "You have brains in your head."
+        You have feet in your shoes. 
+        You can steer yourself any direction you choose. -Dr. Seuss";
+        System.out.println(output);
+    }
+    
+}
